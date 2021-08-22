f9f15255717a9eb94313d01fbdbbe4bf4739ac9f
tirou1229 <stvellen@gmail.com>
2021-08-22 04:48:09 +0200

    hotfix

--- script.js
+++ script.js
@@ -1,64 +1,77 @@
 let activetab, score, currentscore, currentdice, dice, hold, finalscore;
+$('.new').click(reset); //New game button 
 
+$('.roll').on('click', function () { // roll dice button function
+    dice = Math.floor(Math.random() * 6) + 1;
+    $("img").attr("src", "images/dice-" + dice + ".png");
 
-    $('.new').click(reset);
+    if (dice !== 1) {
+        currentdice = parseInt($('#current' + activetab).text());
+        currentscore = dice + currentdice;
+        $('#current' + activetab).text(currentscore);
 
-    $('.roll').on('click' , function(){
-                dice = Math.floor(Math.random() * 6) + 1;    
-            $("img").attr("src", "images/dice-" + dice + ".png");   
-            
-                if (dice !== 1){
-                    currentdice = parseInt($('#current' + activetab).text());
-                    currentscore = dice + currentdice;
-                    $('#current' + activetab).text(currentscore);
-                    
-                }
-                else{
-                    nextplayer();
-                }
-            });
-
-    $('.hold').on('click', function(){
-        
-        hold = parseInt($('#score'+ activetab).text());
-        score = currentscore + hold;
-        $('#score' + activetab).text(score);
+    }
+    else {
         nextplayer();
-            
-    })
+    }
+});
+
+$('.hold').on('click', function () { // hold button function
+
+    hold = parseInt($('#score' + activetab).text());
+    score = currentscore + hold;
+    $('#score' + activetab).text(score);
+    nextplayer();
+    if(hold >= 100 || score >= 100){ // check possible win
+        if(activetab == 2){
+            alert('Player1 Win'); 
+            $(".info-text1").text("Player 1 Win");
+            $(".info-text2").empty();           
+        }
+        else{
+            alert('Player2 Win');
+            $(".info-text2").text("Player 2 Win");
+            $(".info-text1").empty(); 
+        }
+    }    
+
+})
 
 
-
-
+    
 
 //function to know how's next
-function nextplayer(){
+function nextplayer() {
 
-    currentscore = 0;
-
-    // set ternary operator
+    currentscore = 0; // set current score to 0 value so as the last value is not stock in memory
 
     activetab === 1 ? activetab = 2 : activetab = 1;
-    if(activetab == 1){
+    if (activetab == 1) {
         $("#active").addClass("activeplayer");
         $("#active1").removeClass("activeplayer");
+        $("#player1").addClass("point");
+        $("#player2").removeClass("point");
         $(".info-text1").text("Player 1 turn");
         $(".info-text2").empty();
         $("#current2").text("0");
+        $("img").attr("src", "images/dice-1.png");
     }
-    else{
+    else {
         $("#active").removeClass("activeplayer");
         $("#active1").addClass("activeplayer");
+        $("#player2").addClass("point");
+        $("#player1").removeClass("point");
         $(".info-text2").text("Player 2 turn");
         $(".info-text1").empty();
         $("#current1").text("0");
-    }  
-   
+        $("img").attr("src", "images/dice-1.png");
+    }
+
 }
 
 // function to reset the game
 
-function reset(){
+function reset() { //New game button function
 
     $("#score1").text("0");
     $("#score2").text("0");
@@ -66,11 +79,10 @@ function reset(){
     $("#current1").text("0");
     $("#current2").text("0");
 
-    $(".player1").removeClass("activeplayer");
-    $(".player2").removeClass("activeplayer");
-
     $(".info-text1").empty();
-    $(".info-text2").empty();    
+    $(".info-text2").empty();
+
+    nextplayer();
 
 }
 
